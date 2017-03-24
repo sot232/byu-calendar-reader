599d2291d1bda28e2d42ff727afdc97786a9716b
timmoffett <timothytmoffett@gmail.com>
2017-03-24 11:44:08 -0600

    Update byu-calendar-reader.php

--- byu-calendar-reader/byu-calendar-reader.php
+++ byu-calendar-reader/byu-calendar-reader.php
@@ -243,7 +243,7 @@ function y_cal_loadXML() {
 		
 		// have to do two queries so we get the right category for the child events. (ex: Geology shows up under CPMS otherwise)
 		$url1 = $options["byu_url"] . 'categories='. $options[first_query] . '&event[min][date]=' . date('Y-m-d') . '&event[max][date]=' . date("Y-m-d", mktime(0, 0, 0, date("m")+$options["getMonths"],   date("d"),   date("Y")+$options["getYears"]));
-		$url2 = $options["byu_url"] . 'categories=all' . '&event[min][date]=' . date('Y-m-d') . '&event[max][date]=' . date("Y-m-d", mktime(0, 0, 0, date("m")+$options["getMonths"],   date("d"),   date("Y")+$options["getYears"]));
+		$url2 = $options["byu_url"] . 'categories='. $options[widget_categories2] . '&event[min][date]=' . date('Y-m-d') . '&event[max][date]=' . date("Y-m-d", mktime(0, 0, 0, date("m")+$options["getMonths"],   date("d"),   date("Y")+$options["getYears"]));
 
 		
 
@@ -273,7 +273,7 @@ function y_cal_loadXML() {
 					$xml = $xml1;
 			}
 			// load each event into an array and add to database
-			foreach($xml2->item as $event)
+			foreach($xml->item as $event)
 			{
 				$current = array(
 					'AllDay' => (int) filter_var($event->AllDay, FILTER_VALIDATE_BOOLEAN),
